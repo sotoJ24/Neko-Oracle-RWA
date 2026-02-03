59ea1c2ced2b06cdc2e54f3dc1ead9cd0c22ac22
Josue Soto <sotoherrerajosue@gmail.com>
2026-02-03 15:53:07 -0600

    fix: Import SchedulerService in prices module

--- apps/ingestor/src/modules/prices.module.ts
+++ apps/ingestor/src/modules/prices.module.ts
@@ -3,6 +3,7 @@ import { PricesController } from '../controllers/prices.controller';
 import { PriceFetcherService } from '../services/price-fetcher.service';
 import { StockService } from '../services/stock.service';      
 import { FinnhubAdapter } from '../providers/finnhub.adapter'; 
+import { SchedulerService } from '../services/scheduler.service';
 
 @Module({
   controllers: [PricesController],
